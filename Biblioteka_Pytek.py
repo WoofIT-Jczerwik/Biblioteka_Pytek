f77170d2bac160c71ecedd43b763b977369b6226
jakub <jakub@STACJONARA>
2026-05-31 18:33:41 +0200

    Dodaj pliki projektów.

---
+++ Biblioteka_Pytek.py
@@ -0,0 +1,164 @@
+# -*- coding: cp1250 -*-
+class Book:
+    # Zmienna klasowa dzia³aj¹ca analogicznie do statycznej listy w C#
+    zbiory = []
+
+    def __init__(self, title, author):
+        self.id = len(Book.zbiory)
+        self.tytul = title
+        self.autor = author
+        self.wypozyczona = False # Domyœlnie dodawana ksi¹¿ka nie jest wypo¿yczona
+
+    @staticmethod
+    def dodajKsiazke(title, author):
+        nowa = Book(title, author)
+        Book.zbiory.append(nowa)
+
+    @staticmethod
+    def wyswietlKsiazki():
+        print("\n\n=== Dostêpne ksi¹¿ki ===")
+        for i, ksiazka in enumerate(Book.zbiory):
+            if ksiazka is not None:
+                status = "Wypo¿yczona" if ksiazka.wypozyczona else "Dostêpna"
+                print(f"{i}. {ksiazka.tytul} - {ksiazka.autor} - ID Ksi¹¿ki: {i} [{status}]")
+
+
+class Person:
+    # Zmienna klasowa dzia³aj¹ca analogicznie do statycznej listy w C#
+    klienci = []
+
+    def __init__(self, name):
+        self.imieNazwisko = name
+        self.wypozyczone = []
+
+    @staticmethod
+    def nowyKlient(name):
+        nowy = Person(name)
+        Person.klienci.append(nowy)
+        print(f"Dodano nowego klienta: {name}")
+
+    @staticmethod
+    def wypozyczKsiazke(pozyczajacy, idKsiazki):
+        if len(pozyczajacy.wypozyczone) < 10:
+            pozyczajacy.wypozyczone.append(Book.zbiory[idKsiazki])
+            Book.zbiory[idKsiazki].wypozyczona = True # Oznaczamy ksi¹¿kê jako wypo¿yczon¹
+        else:
+            print("Nie mo¿na wypo¿yczyæ wiêcej ni¿ 10 ksi¹¿ek.")
+
+    def zwrocKsiazke(self, zwracajacy, idKsiazki):
+        ksiazka = Book.zbiory[idKsiazki]
+        if ksiazka in zwracajacy.wypozyczone:
+            zwracajacy.wypozyczone.remove(ksiazka)
+            ksiazka.wypozyczona = False # Oznaczamy ksi¹¿kê jako dostêpna
+        else:
+            print("Ta ksi¹¿ka nie jest wypo¿yczona przez tego klienta.")
+
+    @staticmethod
+    def wyswietlWypozyczone(id):
+        klient = Person.klienci[id]
+        print(f"\n\n=== Wypo¿yczone ksi¹¿ki przez {klient.imieNazwisko} ===")
+        for i, ksiazka in enumerate(klient.wypozyczone):
+            idKsiazki = Book.zbiory.index(ksiazka)
+            print(f"{i+1}. {ksiazka.tytul} - {ksiazka.autor} - ID Ksi¹¿ki: {idKsiazki}")
+
+    @staticmethod
+    def wyswietlKlientow():
+        print("\n\n=== Wypo¿yczaj¹cy ===")
+        for i, klient in enumerate(Person.klienci):
+            print(f"{i}. {klient.imieNazwisko}")
+
+
+def populator():
+    # Funkcja, aby nie trzeba by³o przy ka¿dym odpaleniu "z palca" populowaæ
+    Person.nowyKlient("Spejson")
+    Person.nowyKlient("Wojtas")
+    Person.nowyKlient("KubaC")
+    Person.nowyKlient("Bibliotekarz")
+    
+    Book.dodajKsiazke("Chlopi", "Reymont")
+    Person.wypozyczKsiazke(Person.klienci[0], 0)
+    
+    Book.dodajKsiazke("Ogniem i Mieczem", "Sienkiewicz")
+    Person.wypozyczKsiazke(Person.klienci[0], 1)
+    
+    Book.dodajKsiazke("Naprawa Poloneza", "FSO")
+    Person.wypozyczKsiazke(Person.klienci[1], 2)
+    
+    Book.dodajKsiazke("Jak zdac cwiczenia z PPO?", "Praca zbiorowa studentow")
+    Person.wypozyczKsiazke(Person.klienci[2], 3)
+    
+    Book.dodajKsiazke("C# dla opornych", "Jakiœ Programista")
+
+
+def menu():
+    print("\n\n=== Biblioteka => Projekt Jakuba Czerwika ===")
+    print("Co chcesz zrobic?")
+    print("1. Dodaj nowego Uzytkownika")
+    print("2. Wypo¿ycz/zwróæ ksi¹¿kê")
+    print("3. Raporty")
+    print("4. Zakoncz program")
+    
+    wybor_str = input()
+    try:
+        wybor = int(wybor_str)
+        if wybor >= 5:
+            wybor = 0
+    except ValueError:
+        wybor = 0 # Niepoprawny wybór, ustawiamy na 0, aby nie by³o wyj¹tku
+        
+    return wybor
+
+
+def main():
+    populator()
+    opcja = 0
+    
+    while opcja != 4:
+        opcja = menu()
+        
+        if opcja == 1:
+            print("Podaj imiê i nazwisko nowego klienta")
+            Person.nowyKlient(input())
+            
+        elif opcja == 2:
+            print("Naciœnij 1 aby wypo¿yczyæ ksi¹¿kê lub dowolny inny klawisz ¿eby j¹ zwróciæ")
+            wypozyczZwroc = input()
+            
+            if wypozyczZwroc == "1":
+                Person.wyswietlKlientow()
+                print("Podaj id klienta")
+                idKlienta = int(input())
+                print("Podaj id ksi¹¿ki")
+                Book.wyswietlKsiazki()
+                idKsiazki = int(input())
+                Person.wypozyczKsiazke(Person.klienci[idKlienta], idKsiazki)
+            else:
+                Person.wyswietlKlientow()
+                print("Podaj id klienta")
+                idKlienta = int(input())
+                Person.wyswietlWypozyczone(idKlienta)
+                print("Podaj id ksi¹¿ki")
+                idKsiazki = int(input())
+                Person.klienci[idKlienta].zwrocKsiazke(Person.klienci[idKlienta], idKsiazki)
+                
+        elif opcja == 3:
+            print("Naciœnij 1 aby zobaczyæ raport dostêpnych ksi¹¿ek lub dowolny inny klawisz ¿eby zobaczyæ raport wypo¿yczeñ klienta")
+            raport = input()
+            
+            if raport == "1":
+                Book.wyswietlKsiazki()
+            else:
+                Person.wyswietlKlientow()
+                # Zgodnie z C#, brak printa z proœb¹ o wpisanie ID, od razu wczytujemy
+                id_do_raportu = int(input()) 
+                Person.wyswietlWypozyczone(id_do_raportu)
+                
+        elif opcja == 4:
+            pass # Pêtla zakoñczy siê po tym kroku
+            
+        else:
+            print("Niepoprawna opcja, spróbuj ponownie.")
+
+
+if __name__ == "__main__":
+    main()
